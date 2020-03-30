09c7484f87163153078841edcb02c10c9155f703
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-03-30 14:18:02 -0400

    created the animations for the buttons
    created the animations for the buttons and the script to make them move according to those animations

---
+++ WWUnityPort/Assets/Scripts/UI/MenuUIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUIController : MonoBehaviour
+{
+    public Button[] buttons;
+
+    bool clicked = false;
+
+    public void OnClick()
+    {
+        clicked = !clicked;
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponent<Animator>().SetBool("isActive", clicked);
+        }
+    }
+}
