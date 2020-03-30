3db5369031d0dec1d35663dd97d38fc834c03fe7
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-03-30 14:57:28 -0400

    fixed an issue with the close buttons on some UI elements
    fixed an issue where the buttons weren't working as they should be

---
+++ WWUnityPort/Assets/Scripts/UI/PlayerUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    public GameObject inv;
+    public GameObject shopUI;
+
+    bool invClicked = false;
+
+    public void ToggleInventory()
+    {
+        invClicked = !invClicked;
+        if (inv)
+            inv.SetActive(invClicked);
+    }
+
+    public void CloseShop()
+    {
+        if (shopUI)
+            shopUI.SetActive(false);
+    }
+
+}
