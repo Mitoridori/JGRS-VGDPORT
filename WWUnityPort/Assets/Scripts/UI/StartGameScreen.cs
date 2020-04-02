de782fb7fa4173eb32953d35229750ba36e2b162
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-04-02 15:37:31 -0400

    created the base for the main menu and the start game screen, updated build settings, created the scripts for the main menus
    created the basics for the main menu and start game screen
    
    updated the build settings for the project to allow for the game to flow accurately, created the animations for the buttons on the main menu and for the text on the start game screen
    
    updated the buttons asset so that the icons were all lined up correctly

---
+++ WWUnityPort/Assets/Scripts/UI/StartGameScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScreen : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            Invoke("LoadScene", 0.5f);
+        }
+    }
+
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
