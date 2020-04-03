c10b846cf9a02f597d103715e0e7c53cb012dcb4
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-04-03 14:23:44 -0400

    added quit button and quit confirmation window, changed all text to use the correct fonts now, added the quitwindow tag to tag manager, added log to start screen
    added the quit game button to the game screen and created the prefab for the window used to confirm if you would like to quit the game or not, added the script to control that window from being open and if the buttons are clicked, added the game logo to the start game screen, changed all the text in appropriate places to be the correct font now

---
+++ WWUnityPort/Assets/Scripts/UI/QuitUIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitUIController : MonoBehaviour
+{
+    GameObject quitWindow;
+    bool isActive;
+
+    private void Awake()
+    {
+        isActive = true;
+        quitWindow = GameObject.FindGameObjectWithTag("QuitWindow");
+        quitWindow.SetActive(!isActive);
+    }
+
+    public void ToggleQuitWindow()
+    {
+        isActive = !isActive;
+        quitWindow.SetActive(!isActive);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
