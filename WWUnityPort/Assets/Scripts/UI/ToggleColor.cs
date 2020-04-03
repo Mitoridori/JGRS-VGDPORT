007534d6adfb8eb0d5559c714f16b5913d43aec5
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-04-03 15:27:21 -0400

    removed unneeded folders for deleted levels, started adding quest indicators
    started to add quest indicators to the game, further testing and implementing to be done

---
+++ WWUnityPort/Assets/Scripts/UI/ToggleColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleColor : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer[] meshRenderers;
+
+    [SerializeField]
+    private Material taken;
+
+    [SerializeField]
+    private Material available;
+
+    public void SetIconMaterialTaken()
+    {
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            renderer.material = taken;
+        }
+    }
+
+    public void SetIconMaterialAvailable()
+    {
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            renderer.material = available;
+        }
+    }
+    
+}
