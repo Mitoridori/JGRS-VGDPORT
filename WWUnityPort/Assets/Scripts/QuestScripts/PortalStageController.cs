684e455c8897e162e7c3ea98800f275c66347758
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-04-07 14:18:59 -0400

    created the controller for the portal, minor adjustments to the UI
    adjusted the font color on the quest giver UI for ease of reading
    
    created the script for going through the stages of the portal to be called from the quests

---
+++ WWUnityPort/Assets/Scripts/QuestScripts/PortalStageController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalStageController : MonoBehaviour
+{
+    public GameObject[] portals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        portals = new GameObject[gameObject.transform.childCount];
+
+        for (int i = 0; i < portals.Length; i++)
+        {
+            portals[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void SwitchPortalStage()
+    {
+        for (int i = 0; i < portals.Length; i++)
+        {
+            if(portals[i].activeInHierarchy)
+            {
+                portals[i].SetActive(false);
+                if (i + 1 >= portals.Length)
+                    break;
+                portals[i + 1].SetActive(true);
+                return;
+            }
+        }
+    }
+}
